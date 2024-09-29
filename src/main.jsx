a31b9eca8fb2bf070ee8caf1bcea3273822f21bd
NathanMwape <18mm272@esisalama.org>
2024-09-29 23:43:04 +0200

    service worker

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// Si tu utilises Vite PWA, le service worker sera disponible après la compilation.
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then((registration) => {
+        console.log('Service Worker enregistré avec succès:', registration);
+      })
+      .catch((registrationError) => {
+        console.log('Échec de l\'enregistrement du Service Worker:', registrationError);
+      });
+  });
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
